007b9de959449ba00fd1ba95d6a7d100b8fb5c6b
Jefferson Diaz <djefferson545@gmail.com>
2023-08-24 17:02:43 -0700

    made canges to migrate to kubernetes cloud
    - remove eureka service discovery and config service set up as well as
      its dependencies

--- src/main/java/com/chatapp/chatservice/ChatServiceApplication.java
+++ src/main/java/com/chatapp/chatservice/ChatServiceApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableCaching
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @EnableFeignClients
 @EnableRedisHttpSession
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
